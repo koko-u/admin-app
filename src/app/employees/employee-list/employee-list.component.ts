e07265bc5c0e9f03cc760592d3b495b6c051775c
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2021-11-15 20:30:30 +0900

    display employee list

--- src/app/employees/employee-list/employee-list.component.ts
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from "../employee.model"
+import { of } from "rxjs"
+import { employees } from "../mock/employees"
 
 @Component({
   selector: 'app-employee-list',
@@ -9,12 +11,19 @@ import { Employee } from "../employee.model"
 export class EmployeeListComponent implements OnInit {
 
   employee: Employee;
+  employeeList: Employee[];
 
   constructor() {
     this.employee = new Employee(1, '田中 次郎');
+    this.employeeList = [];
   }
 
   ngOnInit(): void {
+    this.fetchEmployeeList();
   }
 
+  fetchEmployeeList(): void {
+    of(employees)
+      .subscribe(employees => this.employeeList = employees);
+  }
 }
---
+++ src/app/employees/mock/employees.ts
@@ -0,0 +1,14 @@
+import { Employee } from "../employee.model"
+
+export const employees: Employee[] = [
+  new Employee(11, '武山 岳大'),
+  new Employee(12, '駒倉 光紀'),
+  new Employee(13, '長田 研太'),
+  new Employee(14, '高藤 友梨香'),
+  new Employee(15, '浜崎 貴之'),
+  new Employee(16, '緑川 睦'),
+  new Employee(17, '森谷 怜奈'),
+  new Employee(18, '大槻 祐大'),
+  new Employee(19, '岩野 紀之'),
+  new Employee(20, '佐々木 小次郎')
+]
