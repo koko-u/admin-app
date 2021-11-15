1692ba7ec94de3ad712be5cb5360bab5a5719659
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2021-11-15 20:52:29 +0900

    create employee service

--- src/app/employees/employee.service.ts
+++ src/app/employees/employee.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { EmployeesModule } from "./employees.module"
+import { Observable, of } from "rxjs"
+import { Employee } from "./employee.model"
+import { employees } from "./mock/employees"
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: EmployeesModule
 })
 export class EmployeeService {
 
   constructor() { }
+
+  getEmployeeList(): Observable<Employee[]> {
+    return of(employees)
+  }
 }
