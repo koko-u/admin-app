a35b24c63883f7f1ca58fd8bff8aa7cababa42a3
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2021-11-16 22:14:39 +0900

    ng g m in-memory-api --module=app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppComponent } from './app.component';
 import { EmployeesModule } from "./employees/employees.module"
 import { OperationLogsModule } from "./operation-logs/operation-logs.module"
 import { AppRoutingModule } from "./app-routing.module";
-import { DashboardModule } from './dashboard/dashboard.module'
+import { DashboardModule } from './dashboard/dashboard.module';
+import { InMemoryApiModule } from './in-memory-api/in-memory-api.module'
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { DashboardModule } from './dashboard/dashboard.module'
     EmployeesModule,
     OperationLogsModule,
     DashboardModule,
+    InMemoryApiModule,
   ],
   bootstrap: [AppComponent]
 })
