69f0d4223e3e013dc4b1c3cf317efd8673b931b7
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2021-11-20 17:23:57 +0900

    ng g c dashboard/employee-search

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
-import { AppRoutingModule } from "../app-routing.module"
+import { AppRoutingModule } from "../app-routing.module";
+import { EmployeeSearchComponent } from './employee-search/employee-search.component'
 
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    EmployeeSearchComponent
   ],
   imports: [
     CommonModule,
